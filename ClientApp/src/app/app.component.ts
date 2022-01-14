0dd8b9683baa6b6fa3a45ae805533b5761b58f9f
R221 <rcjames1111@gmail.com>
2022-01-14 18:59:42 +0900

    Modify home template

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: []
 })
 export class AppComponent {
   
   @Input() pictures: any;
 
-  
-   
 }
